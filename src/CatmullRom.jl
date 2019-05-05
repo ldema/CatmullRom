6d9e6005fe1d38d5fe27354dc37e527a85313086
ldema <luigi.demaio@gmail.com>
2019-05-05 17:10:12 +0200

    Update CatmullRom.jl

--- src/CatmullRom.jl
+++ src/CatmullRom.jl
@@ -75,7 +75,6 @@ function testCatmullRom()
     V=Vertex2D[Vertex2D(2.0,2.0),Vertex2D(4.0,3.0),Vertex2D(8.0,2.0),Vertex2D(3.0,1.0)]
     CM=CalculateCatmullRom(V)
     showCatmullRom(CM)
-    savefig("CatmullRom.png")
 end
 
 end
