18a9a5dd823fc7fe849075c3df1cefe04ea3756c
ldema <luigi.demaio@gmail.com>
2019-05-06 18:12:44 +0200

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,16 @@
-using CatmullRom
 
-V=Vertex2D[Vertex2D(2.0,2.0),Vertex2D(4.0,3.0),Vertex2D(8.0,2.0),Vertex2D(3.0,1.0)]
-CM=CalculateCatmullRom(V)
-showCatmullRom(CM)
+
+
+
+using Test, CatmullRom
+
+#setup code
+
+@testset "Test 1" begin
+    @test 4 == 2+2
+    @test_throws DomainError (-1)^0.5
+end;
+
+@testset "Test 2" begin
+    @test 1+1 == 2
+end;
