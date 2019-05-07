f7b0443ce9073472ab41a81f5f5b8fbc880879d0
ldema <luigi.demaio@gmail.com>
2019-05-07 15:46:22 +0200

    Remove Winston and )

--- src/CatmullRom.jl
+++ src/CatmullRom.jl
@@ -75,7 +75,7 @@ end
 function testCatmullRom()
     V=Vertex2D[Vertex2D(2.0,2.0),Vertex2D(4.0,3.0),Vertex2D(8.0,2.0),Vertex2D(3.0,1.0)]
     CM=CalculateCatmullRom(V)
-    showCatmullRom(CM)
+    #showCatmullRom(CM)
 end
 
 end
