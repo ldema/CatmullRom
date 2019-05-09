62e4fb38df7872157d830c4c711eab453e5d45e2
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-05-09 15:12:07 +0000

    build based on 325c3a7

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#CatmullRom.jl-1","page":"CatmullRom.jl","title":"CatmullRom.jl","text":"","category":"section"},{"location":"#","page":"CatmullRom.jl","title":"CatmullRom.jl","text":"Documentation for CatmullRom.jl","category":"page"},{"location":"#","page":"CatmullRom.jl","title":"CatmullRom.jl","text":"showCatmullRom(V::Array{Vertex2D})","category":"page"}]
+[{"location":"#CatmullRom.jl-1","page":"CatmullRom.jl","title":"CatmullRom.jl","text":"","category":"section"},{"location":"#","page":"CatmullRom.jl","title":"CatmullRom.jl","text":"testCatmullRom()","category":"page"},{"location":"#CatmullRom.testCatmullRom-Tuple{}","page":"CatmullRom.jl","title":"CatmullRom.testCatmullRom","text":"Custom length docs for T.\n\n\n\n\n\n","category":"method"},{"location":"#","page":"CatmullRom.jl","title":"CatmullRom.jl","text":"Documentation for CatmullRom.jl","category":"page"}]
 }
