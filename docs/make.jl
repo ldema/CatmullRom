c330663b9ec79d4a60399e6101243f2dec8c19aa
ldem <luigi.demaio@gmail.com>
2019-05-09 14:20:25 +0200

    t

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,2 @@
 using Documenter
 using CatmullRom
-
-makedocs(
-    sitename = "CatmullRom",
-    format = :html,
-    modules = [CatmullRom]
-)
-
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-deploydocs(
-    repo = "github.com/ldema/CatmullRom.jl"
-    branch = "gh-pages"
-)
