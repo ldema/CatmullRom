f0aa7b476ee3fa37de5656454bc94d9fd3f60496
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-05-10 08:57:28 +0000

    build based on 2beaf9b

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#CatmullRom.jl-1","page":"CatmullRom.jl","title":"CatmullRom.jl","text":"","category":"section"},{"location":"#","page":"CatmullRom.jl","title":"CatmullRom.jl","text":"testCatmullRom()","category":"page"},{"location":"#","page":"CatmullRom.jl","title":"CatmullRom.jl","text":"Documentation for CatmullRom.jl","category":"page"}]
+[{"location":"#CatmullRom.jl-1","page":"CatmullRom.jl","title":"CatmullRom.jl","text":"","category":"section"},{"location":"#","page":"CatmullRom.jl","title":"CatmullRom.jl","text":"CurrentModule=CatmullRom","category":"page"},{"location":"#","page":"CatmullRom.jl","title":"CatmullRom.jl","text":"testCatmullRom()","category":"page"},{"location":"#","page":"CatmullRom.jl","title":"CatmullRom.jl","text":"{docs} Documentation for CatmullRom.jl (Documenter)","category":"page"}]
 }
